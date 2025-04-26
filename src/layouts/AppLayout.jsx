5712ee4c4dc2488ee3114ad2065acbe5496c7064
nmmartinezp <nikimartin56@gmail.com>
2025-04-26 15:42:06 -0400

    update scroll

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,7 +1,7 @@
 function AppLayout({ nav, page, footer }) {
   return (
     <div
-      className={`dark text-foreground bg-background h-screen w-full overflow-y-scroll`}
+      className={`dark text-foreground bg-background h-screen w-full overflow-y-scroll scrollbar-hide`}
     >
       {nav}
       <main className={`w-full h-screen p-8`}>{page}</main>
