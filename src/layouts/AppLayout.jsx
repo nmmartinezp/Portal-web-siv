1505472fcd5928810e669924ee6ba638b01ad006
nmmartinezp <nikimartin56@gmail.com>
2025-04-26 15:11:34 -0400

    update temp

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,7 +1,7 @@
 function AppLayout({ nav, page, footer }) {
   return (
     <div
-      className={`my-theme text-foreground bg-background h-screen w-full overflow-y-scroll`}
+      className={`dark text-foreground bg-background h-screen w-full overflow-y-scroll`}
     >
       {nav}
       <main className={`w-full h-screen p-8`}>{page}</main>
