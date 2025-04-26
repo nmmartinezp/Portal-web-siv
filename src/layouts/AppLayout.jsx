d73a0fbd04964abd4bdf94ed1bf86260c3a37f19
nmmartinezp <nikimartin56@gmail.com>
2025-04-25 20:45:47 -0400

    update app layput for my theme

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,10 +1,7 @@
 function AppLayout({ nav, page, footer }) {
-  const modeTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
   return (
     <div
-      className={`${modeTheme} text-foreground bg-background h-screen w-full overflow-y-scroll`}
+      className={`my-theme text-foreground bg-background h-screen w-full overflow-y-scroll`}
     >
       {nav}
       <main className={`w-full h-screen p-8`}>{page}</main>
