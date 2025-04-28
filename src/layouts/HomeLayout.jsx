bba83fb770a02dfdad56fd54737d1e018cccf67b
nmmartinezp <nikimartin56@gmail.com>
2025-04-27 20:55:32 -0400

    add scroll section

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -16,21 +16,45 @@ function HomeLayout({
       <section className="w-full h-[93vh] px-2 flex items-start justify-center snap-start">
         {carosusel}
       </section>
-      <section className="w-full h-[93vh] px-2 flex items-center justify-center snap-start">
-        <div>{pt01}</div>
-        <article>{art01}</article>
+      <section className="w-full h-[93vh] flex items-center justify-center px-2 snap-start">
+        <div className="w-11/12 h-[78%] md:h-[93vh] flex flex-col md:flex-row items-center justify-center">
+          <div className="flex items-center justify-center pt-10 p-4 w-11/12 md:w-[40%] h-[40%] md:h-full">
+            {pt01}
+          </div>
+          <article className="flex items-center justify-center p-4 md:w-[60%] h-[60%] md:h-full">
+            {art01}
+          </article>
+        </div>
       </section>
-      <section className="w-full h-[93vh] px-2 flex items-center justify-center snap-start">
-        <article>{art02}</article>
-        <div>{pt02}</div>
+      <section className="w-full h-[93vh] flex items-center justify-center px-2 snap-start">
+        <div className="w-11/12 h-[78%] md:h-[93vh] flex flex-col md:flex-row items-center justify-center">
+          <article className="flex items-center justify-center p-4 md:w-[60%] h-[60%] md:h-full">
+            {art02}
+          </article>
+          <div className="flex items-center justify-center p-4 w-11/12 md:w-[40%] h-[40%] md:h-full">
+            {pt02}
+          </div>
+        </div>
       </section>
-      <section className="w-full h-[93vh] px-2 flex items-center justify-center snap-start">
-        <div>{pt03}</div>
-        <article>{art03}</article>
+      <section className="w-full h-[93vh] flex items-center justify-center px-2 snap-start">
+        <div className="w-11/12 h-[78%] md:h-[93vh] flex flex-col md:flex-row items-center justify-center">
+          <div className="flex items-center justify-center p-4 w-11/12 md:w-[40%] h-[40%] md:h-full">
+            {pt03}
+          </div>
+          <article className="flex items-center justify-center p-4 md:w-[60%] h-[60%] md:h-full">
+            {art03}
+          </article>
+        </div>
       </section>
-      <section className="w-full h-[93vh] px-2 flex items-center justify-center snap-start">
-        <article>{art04}</article>
-        <div>{pt04}</div>
+      <section className="w-full h-[93vh] flex items-center justify-center px-2 snap-start">
+        <div className="w-11/12 h-[78%] md:h-[93vh] flex flex-col md:flex-row items-center justify-center">
+          <article className="flex items-center justify-center p-4 md:w-[60%] h-[60%] md:h-full">
+            {art04}
+          </article>
+          <div className="flex items-center justify-center p-4 w-11/12 md:w-[40%] h-[40%] md:h-full">
+            {pt04}
+          </div>
+        </div>
       </section>
     </>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,56 @@
 import HomeLayout from "../../layouts/HomeLayout";
 import Carousel from "../../components/Carousel";
+import { Image } from "@heroui/react";
 import useDataCarousel from "../../hooks/useDataCarousel";
+import catedral02Siv from "../../assets/catedral02Siv.webp";
 
 function Home() {
   const dataCarousel = useDataCarousel();
-  return <HomeLayout carosusel={<Carousel data={dataCarousel} />} />;
+  return (
+    <HomeLayout
+      carosusel={<Carousel data={dataCarousel} />}
+      pt01={
+        <Image
+          isBlurred
+          isZoomed
+          alt="Iglesia Catedral de San Ignacio de Velasco"
+          src={catedral02Siv}
+          className="w-full h-[230px] md:h-[350px]"
+        />
+      }
+      art01={<span>Hola</span>}
+      pt02={
+        <Image
+          isBlurred
+          isZoomed
+          alt="Iglesia Catedral de San Ignacio de Velasco"
+          src={catedral02Siv}
+          className="w-full h-[230px] md:h-[350px]"
+        />
+      }
+      art02={<span>Hola</span>}
+      pt03={
+        <Image
+          isBlurred
+          isZoomed
+          alt="Iglesia Catedral de San Ignacio de Velasco"
+          src={catedral02Siv}
+          className="w-full h-[230px] md:h-[350px]"
+        />
+      }
+      art03={<span>Hola</span>}
+      pt04={
+        <Image
+          isBlurred
+          isZoomed
+          alt="Iglesia Catedral de San Ignacio de Velasco"
+          src={catedral02Siv}
+          className="w-full h-[230px] md:h-[350px]"
+        />
+      }
+      art04={<span>Hola</span>}
+    />
+  );
 }
 
 export default Home;
