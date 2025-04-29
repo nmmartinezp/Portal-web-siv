464c2a711b07cbe4982add0aff41f88c1d98a825
nmmartinezp <nikimartin56@gmail.com>
2025-04-29 16:43:06 -0400

    create add Title updater component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { Outlet } from "react-router";
 import AppLayout from "@layouts/AppLayout";
 import NavBar from "@components/NavBar";
 import Footer from "@components/Footer";
+import TitleUpdater from "@components/TitleUpdater";
 import "./App.css";
 
 function App() {
-  return <AppLayout nav={<NavBar />} page={<Outlet />} footer={<Footer />} />;
+  return (
+    <>
+      <TitleUpdater />
+      <AppLayout nav={<NavBar />} page={<Outlet />} footer={<Footer />} />
+    </>
+  );
 }
 
 export default App;
