2126e3bba54e5bef28feaffd8180c150f1071e0b
nmmartinezp <nikimartin56@gmail.com>
2025-05-01 17:01:13 -0400

    update component navbar - button mobile color

--- src/components/NavBar/NavBarItem.jsx
+++ src/components/NavBar/NavBarItem.jsx
@@ -3,7 +3,7 @@ import { NavbarItem, NavbarMenuItem, Button } from "@heroui/react";
 function NavBarItem({ children, variant, onNavigate }) {
   return variant === "mobile" ? (
     <NavbarMenuItem>
-      <Button className="w-full font-bold" color="primary" onPress={onNavigate}>
+      <Button className="w-full font-bold bg-warning-600" onPress={onNavigate}>
         {children}
       </Button>
     </NavbarMenuItem>
